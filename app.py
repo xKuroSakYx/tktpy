991960e38ba6e5f18b3e46916300694ac5585501
xKuroSakYx <cloudshopproyect@gmail.com>
2023-10-08 02:59:39 -0400

    18

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ consumer_secret= 'rQJKpa4p8j8Pc1Ju9llERSDyCcj6NuKwyXrGJy4wHFYcDIU923'
 
 web_url = "https://x6nge.com"
 #web_url = "http://localhost:8080"
+
 request_token_url = "https://api.twitter.com/oauth/request_token"
 access_token_url = "https://api.twitter.com/oauth/access_token"
 
